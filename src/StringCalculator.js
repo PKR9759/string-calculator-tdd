51fc522026c8404e7cf6d4bffc8bd490876169d4
PKR9759 <kuldiprparmar9759@gmail.com>
2025-07-05 15:10:53 +0530

    feat: support custom single-character delimiters in Add method

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,10 +1,20 @@
 class StringCalculator {
     Add(numbers) {
         if (numbers === "") return 0;
-        const delimiters = /[,\n]/;
-        const nums = numbers.split(delimiters).map(Number);
+
+        let delimiter = /[,\n]/;
+
+        if (numbers.startsWith("//")) {
+            const delimiterLineEnd = numbers.indexOf('\n');
+            const delimiterString = numbers.substring(2, delimiterLineEnd);
+            delimiter = new RegExp(`[${delimiterString}]`);
+            numbers = numbers.substring(delimiterLineEnd + 1);
+        }
+
+        const nums = numbers.split(delimiter).map(Number);
         return nums.reduce((sum, num) => sum + num, 0);
     }
 }
+
 
 module.exports = { StringCalculator };
