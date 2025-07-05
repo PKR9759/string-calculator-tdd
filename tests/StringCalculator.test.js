06c239526c2abf96ebef17a9f897b4b8cd193b1b
PKR9759 <kuldiprparmar9759@gmail.com>
2025-07-05 15:02:17 +0530

    test: return sum of two comma-separated numbers

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -9,4 +9,9 @@ test('returns number itself for single number input', () => {
     const calculator = new StringCalculator();
     expect(calculator.Add("5")).toBe(5);
   });
+
+  test('returns sum of two numbers separated by comma', () => {
+    const calculator = new StringCalculator();
+    expect(calculator.Add("1,2")).toBe(3);
+  });
   
